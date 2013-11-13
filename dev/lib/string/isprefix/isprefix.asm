6809759aad6471f12c30fb1ce77433df3545f949
Johnny <johnnystarr@gmail.com>
2013-11-13 17:05:43 -0600

    Added Header to files

--- dev/lib/string/isprefix/isprefix.asm
+++ dev/lib/string/isprefix/isprefix.asm
@@ -1,3 +1,13 @@
+; _|      _|    _|_|      _|_|_|      
+; _|_|  _|_|  _|    _|  _|            
+; _|  _|  _|  _|    _|    _|_|        
+; _|      _|  _|    _|        _|      
+; _|      _|    _|_|    _|_|_|  
+;
+; The [M]inimalist [O]perating [S]ystem
+; Created by Johnny Starr (c) 2013
+; www.thestarrlab.com
+
 org 100h
 
 jmp start
