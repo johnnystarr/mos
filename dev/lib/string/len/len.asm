54f8a2feaa96145da02b73ed6c912c1aac3a6e22
Johnny Starr <johnnystarr@gmail.com>
2013-11-13 15:45:41 -0600

    Update len.asm

--- dev/lib/string/len/len.asm
+++ dev/lib/string/len/len.asm
@@ -5,16 +5,16 @@ jmp start
 ; mos_string_len() returns the length of a string
 ; IN: SI = string
 ; OUT: DX = length
-mos_string_len:         
-    mov dx, 0			; start off count at 0
+mos_string_len:     
+    mov dx, 0         ; start off count at 0
 .count:            
-    cmp [si], byte 0	        ; are we at the end of the string?
-    je .done			; if so, were done here
-    inc dx			; otherwise: increment our count
-    inc si			; move to next character
-    jmp .count			; loop
+    cmp [si], byte 0  ; are we at the end of the string?
+    je .done          ; if so, were done here
+    inc dx            ; otherwise: increment our count
+    inc si            ; move to next character
+    jmp .count        ; loop
 .done:               
-    ret				; return
+    ret               ; return
 
 start:         
     mov si, str
